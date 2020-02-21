f61380b1d80abd13e5f4ab1c0ecc1216719c0ffb
pratamawijaya <set.mnemonix@gmail.com>
2020-02-21 14:35:42 +0700

    add sample multi coroutines

--- app/src/main/java/com/pratamawijaya/coroutinesretrofit/MainActivity.kt
+++ app/src/main/java/com/pratamawijaya/coroutinesretrofit/MainActivity.kt
@@ -11,13 +11,21 @@ import com.pratamawijaya.coroutinesretrofit.data.repository.TodoRepository
 import com.pratamawijaya.coroutinesretrofit.data.viewmodel.DemoWithViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
+import java.util.concurrent.Executors
 import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    lateinit var job: Job
+
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.IO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        job = Job()
 
         val repository = TodoRepository()
 
@@ -32,6 +40,39 @@ class MainActivity : AppCompatActivity() {
         btnViewmodel.setOnClickListener {
             startActivity(Intent(this, DemoWithViewModel::class.java))
         }
+
+
+        // test launch 3 coroutines
+
+        launch {
+            for (i in 1..100) {
+                println("I'm working in thread ${Thread.currentThread().name}")
+                postItem(Item("data $i"))
+            }
+        }
+
+        val threads = 4
+        val myCustomDispatcher = Executors.newFixedThreadPool(threads).asCoroutineDispatcher()
+
+        launch(myCustomDispatcher) {
+            for (i in 1..100) {
+                println("I'm working in thread ${Thread.currentThread().name}")
+                postItem(Item("data $i"))
+            }
+        }
+
+        launch(myCustomDispatcher) {
+            for (i in 1..100) {
+                println("I'm working in thread ${Thread.currentThread().name}")
+                postItem(Item("data $i"))
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+        Log.d("debug", "destroy job ${job.isCancelled}")
     }
 
     private fun showResult(result: Todo) {
@@ -39,4 +80,31 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "result $result", Toast.LENGTH_SHORT).show()
     }
 
+    suspend fun postItem(item: Item) {
+        val token = requestToken()
+        val post = createPost(token, item)
+        processPost(post)
+    }
+
+    suspend fun requestToken(): Token {
+        val rnds = (0..50).random()
+        delay(500L)
+        return Token("mytoken_$rnds")
+    }
+
+    suspend fun createPost(token: Token, item: Item): Post {
+        val rnds = (10..5000).random()
+        delay(500L)
+        return Post("token : ${token.token} item ${item.data} $rnds")
+    }
+
+    fun processPost(post: Post) {
+        Log.d("tag", "debug post ${post.title}")
+    }
 }
+
+data class Item(val data: String)
+
+data class Post(val title: String)
+
+data class Token(val token: String)
